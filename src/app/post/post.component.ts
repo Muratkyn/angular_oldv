b591b2ddc34933aa609510ccc9414b06b366511c
Murat Koyun <muratk1845@gmail.com>
2024-02-22 16:01:22 +0100

    angular_app_services

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CourseService } from '../courses.service';
 
 @Component({
   selector: 'app-post',
@@ -7,13 +8,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  title: string = "List of Titles"
+  title: string = "List of Titles";
+  courses;
   postlist1: string = "A postlist to child"
 
   @Input() message!:string;
   
   constructor() {
-
+    let service = new CourseService()
+    this.courses = service.getCourses();
   }
 
   ngOnInit(): void {
