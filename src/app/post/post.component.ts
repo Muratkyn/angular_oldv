6b4234954bdb278e8e24541214035fe76245c1d4
Murat Koyun <muratk1845@gmail.com>
2024-02-22 14:56:03 +0100

    angular_app_input_decorator2

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
 
   title: string = "List of Titles"
+  postlist1: string = "A postlist to child"
 
   @Input() message!:string;
   
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
 
---
+++ src/app/postlist/postlist.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-postlist',
+  templateUrl: './postlist.component.html',
+  styleUrl: './postlist.component.css'
+})
+export class PostlistComponent {
+
+  @Input() postlist!: string;
+}
